ce044b7ab3e7272b7137e75cc10b6014a1e39da5
EmulRoss <emul.ross@gmail.com>
2018-04-24 18:31:44 +0200

    Add login console

---
+++ views/login/index.js
@@ -0,0 +1,19 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+let mainRouter = require('./mainRoutes');
+
+app.use('/', mainRouter);
+
+app.listen(process.env.PORT || 3000);
+console.log("Express server running on port 3000");
+
+function AttemptLogin()
+{
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    console.log(username)
+    console.log(password)
+}
+
