c85de9881a806f59f92a5ef773c913f58437e087
EmulRoss <emul.ross@gmail.com>
2018-04-24 22:06:25 +0200

    Main change

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-let path = require('path');
-let express = require('express');
-let app = express();
-let mainRouter = require('./mainRoutes');
-
-app.use('/', mainRouter);
-
-app.listen(process.env.PORT || 3000);
-console.log("Express server running on port 3000");
+window.location.href = '../login/view.html';
