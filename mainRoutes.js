ec3de8558a9299842c9002bd4f5629a37fdd3efe
EmulRoss <emul.ross@gmail.com>
2018-04-24 21:54:40 +0200

    Update

--- mainRoutes.js
+++ mainRoutes.js
@@ -9,7 +9,7 @@ mainRouter.get('/',function(req, res) {
     res.send('Hello World, I\'m Node.js');
     });
 
-mainRouter.get('/login',function(req, res){
+mainRouter.get('/views',function(req, res){
     res.sendFile(path.join(__dirname,'views','login/view.html'));
 });
 module.exports = mainRouter;
