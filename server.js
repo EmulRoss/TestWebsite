e50c97f24504a9e9378ef13067b4a65b39b44d78
EmulRoss <emul.ross@gmail.com>
2018-04-25 18:07:09 +0200

    Attempt6

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*let path = require('path');
 let express = require('express');
-let app = express();
+
 let mainRouter = require('./mainRoutes');
 
 app.use('/', mainRouter);
@@ -8,11 +8,14 @@ app.use('/', mainRouter);
 app.listen(process.env.PORT || 3000);
 console.log("Express server running on port 3000");*/
 
-//var port = process.env.PORT || config.APP_PORT
+
 
 let path = require('path');
 let express = require('express');
 let mainRouter = express.Router();
+let app = express();
+
+app.listen(process.env.PORT || 3000);
 
 mainRouter.get('/view',function(req, res){
     res.sendFile(path.join(__dirname,'views', 'login', 'view.html'));
