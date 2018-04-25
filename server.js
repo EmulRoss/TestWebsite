6862f15baf3bbd2794d372ba8c924c1128f4d007
EmulRoss <emul.ross@gmail.com>
2018-04-25 12:47:44 +0200

    Attempt3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let path = require('path');
 let express = require('express');
 let mainRouter = express.Router();
 
-mainRouter.get('/views',function(req, res){
+mainRouter.get('views',function(req, res){
     res.sendFile(path.join(__dirname,'views','login/view.html'));
 });
 module.exports = mainRouter;
